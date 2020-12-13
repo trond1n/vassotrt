790259dbb21a8de6a9b05e517a2945ba81339e31
Yuri Trondin <lopata@2ch.hk>
2020-12-13 14:22:56 +0500

    причесал код

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,54 @@
-// const flky = new Flickity(".feedback__list", {
-//   cellAlign: "center",
-//   freeScroll: true,
-//   wrapAround: false,
-// });
-
+//константы
 const utils = window.fizzyUIUtils;
 
 const carousel = document.querySelector(".feedback__list");
 const flkty = new Flickity(carousel, {
-    
   prevNextButtons: false,
   pageDots: false,
 });
-// elements
-const cellsButtonGroup = document.querySelector('.button-group--cells');
-const cellsButtons = utils.makeArray( cellsButtonGroup.children );
+const cellsButtonGroup = document.querySelector(".button-group--cells");
+const cellsButtons = utils.makeArray(cellsButtonGroup.children);
 
-// update buttons on select
-flkty.on( 'select', function() {
-  const previousSelectedButton = cellsButtonGroup.querySelector('.is-selected_paginate');
-  const selectedButton = cellsButtonGroup.children[ flkty.selectedIndex ];
-  previousSelectedButton.classList.remove('is-selected_paginate');
-  selectedButton.classList.add('is-selected_paginate');
-});
 
-//count
+//счетчик
 const itemList = document.querySelectorAll(".feedback__item");
 const field = document.querySelector(".slider__page-count");
-let count = 1
+let count = 1;
 
-// cell select
-cellsButtonGroup.addEventListener( 'click', function( event ) {
-    if ( !matchesSelector( event.target, '.pagination__button' ) ) {
-      return;
-    }
-    var index = cellsButtons.indexOf( event.target );
-    flkty.select( index );
-  });
-// previous
+// кругляши
+flkty.on("select", function () {
+  const previousSelectedButton = cellsButtonGroup.querySelector(
+    ".is-selected_paginate"
+  );
+  const selectedButton = cellsButtonGroup.children[flkty.selectedIndex];
+  previousSelectedButton.classList.remove("is-selected_paginate");
+  selectedButton.classList.add("is-selected_paginate");
+});
+
+cellsButtonGroup.addEventListener("click", function (event) {
+  if (!matchesSelector(event.target, ".pagination__button")) {
+    return;
+  }
+  var index = cellsButtons.indexOf(event.target);
+  flkty.select(index);
+});
+
+
+// кнопка пред
 const previousButton = document.querySelector(".button--previous");
 previousButton.addEventListener("click", function () {
   flkty.previous();
-  if(count !== 1){
-    count--
+  if (count !== 1) {
+    count--;
     field.innerHTML = `${count} / ${itemList.length}`;
   }
 });
-// next
+// кнопка нэкст
 const nextButton = document.querySelector(".button--next");
 nextButton.addEventListener("click", function () {
   flkty.next();
-  if(count !== 6){
-    count++
+  if (count !== 6) {
+    count++;
     field.innerHTML = `${count} / ${itemList.length}`;
   }
-
 });
